3d4cc2dda355e7aee3bd68517376a12691c171df
tarikochuery <ttarikechuery@gmail.com>
2022-11-17 00:12:49 -0300

    Funcionalidade de alterar o nome de usuário adicionada

--- src/components/Usuario.jsx
+++ src/components/Usuario.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+import {IoPencilOutline} from 'react-icons/io5'
+
+
 function Usuario() {
+  const [username, setUsername] = useState('Catana')
+  
+  const handleClickUsername = () => {
+    const newUsername = prompt('Qual seu novo nome de usuário?')
+    setUsername(newUsername)
+  }
+
   return (
     <header className="profile">
       <div className="profile-img">
-        <img src="assets/catanacomics.png" alt='catanacomics'/>
+        <img src="assets/catanacomics.png" alt={username} />
       </div>
       <div class="profile-info">
         <strong>catanacomics</strong>
-        <p>Catana</p>
+        <div className="user-container">
+          <p>{username}</p>
+          <div className="edit-icon">
+            <IoPencilOutline size='1.2rem' onClick={handleClickUsername}/>
+          </div>
+        </div>
       </div>
     </header>
   );
@@ -14,6 +30,5 @@ function Usuario() {
 
 export { Usuario };
 
-//TODO: Adicionar lápis ao lado do nome
 //TODO: Dar função de edição ao nome do usuário
 //TODO: Dar função de edição à imagem do usuário
